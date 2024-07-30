6dff374806a855a155af8fff73f81d495b8fdd63
Mustafa Redifoglu <mustafaredifoglu@gmail.com>
2024-07-30 13:25:26 +0300

    all tests passed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bedroom bedroom = new Bedroom("mustafa's bedroom",new Wall("east"),new Wall("west"),new Wall("north"),new Wall("south"),new Ceiling(3, PaintColor.WHITE),new Bed("comfort",3,1,1,1),new Lamp(LampType.LAVA,false,7),new Wardrobe(5,5,60.00),new Carpet(30,30,PaintColor.GREEN));
+        System.out.println(bedroom);
     }
 }
